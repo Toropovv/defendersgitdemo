18e6d026afe2afa67d771529d8535f3460fa9c94
Toropovv <iliya.toropov@gmail.com>
2023-01-27 20:38:38 -0500

    Created IlyaMain

---
+++ src/test/java/com/gitexample/IlyaMain.java
@@ -0,0 +1,17 @@
+package com.gitexample;
+
+public class IlyaMain {
+    
+public static void main(String[] args) {
+    
+IlyaMain obj = new IlyaMain();
+obj.print("Ilya");
+
+
+}
+
+public void print(String name){
+    System.out.println(name);
+}
+
+}
